2fb7af56641686593ccbffcc8da10ac8d23c954b
happypatel20 <happypatel20898@gmail.com>
2023-03-31 18:43:34 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
