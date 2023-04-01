a7b28d2b0fbbe7cedb13306c6aa245efcba53c31
happypatel20 <happypatel20898@gmail.com>
2023-04-01 23:15:44 +0530

    added package parcel in app

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h1", {}, "I'm happypatel"),
     React.createElement("h2", {}, "I'm h2 tag"),
   ]),
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
     React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
+  ]), 
 ]);
 console.log(parent)
 const root = ReactDOM.createRoot(document.getElementById("root"));
