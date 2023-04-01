c2dcc6aafdc79c2f636a914d319c64430e302421
happypatel20 <happypatel20898@gmail.com>
2023-04-01 23:33:02 +0530

    added script in package json and started using first syntax of JSX

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm happypatel"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]), 
-]);
-console.log(parent)
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from create element"
+);
+
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    Hello from JSX
+  </h1>
+);
+
+//  jsx -> createElement -> object -> HTML tag [babel]
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(jsxHeading);
