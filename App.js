d8411aceac540c14c513ab676d08f772f040ae1f
happypatel20 <happypatel20898@gmail.com>
2023-04-02 11:25:21 +0530

    removed createElement code

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello from create element"
-);
-
 const jsxHeading = (
   <h1 id="heading" className="head">
     Hello from JSX
