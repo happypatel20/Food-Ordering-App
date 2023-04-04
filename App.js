6105cbe3b725cff63431702ee43608262d5138bb
happypatel20 <happypatel20898@gmail.com>
2023-04-04 10:59:26 +0530

    Functional component and JSX

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (
-  <h1 id="heading" className="head">
-    Hello from JSX
-  </h1>
-);
-
+// component 1. class component  and 2. functional component -> A js function that returns a jsx or react element that is called functional compoenent
 //  jsx -> createElement -> object -> HTML tag [babel]
 
+// Rules of JSX:
+// - can have only single root [react Fragment-]
+// - closing the tag is must 
+// - having camelCase in most cases 
+
+const Title = () => {
+  return <p>Hey this is from title component</p>
+}
+const person = {
+  "name": "Happy",
+  "theme": {
+    "backgroundColor": "blue",
+    "color": "white"
+  }
+}
+const Heading = () => {
+ return ( 
+  <>
+    <h1 style={person.theme}>Hello {person.name} from functional component</h1>
+    <Title />
+  </>
+) 
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<Heading />);
